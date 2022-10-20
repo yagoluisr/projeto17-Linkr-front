6e9177f7054ca264cc128aef941427c254e21c86
Victor Vermaas <vhvermaas@gmail.com>
2022-10-19 21:29:37 -0300

    get timeline, get user

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+import { getUser, getPosts } from "../../services/api";
+import PostCard from "./PostCard";
 import styled from "styled-components";
 import ProfilePic from "../../assets/styles/ProfilePic";
 import Title from "../../assets/styles/Title";
@@ -6,17 +9,38 @@ import FormBox from "./FormBox";
 
 export default function Timeline() {
 
+    const [userImage, setUserImage] = useState()
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const promise = getUser()
+            promise.then((user) => {
+                setUserImage(user.data.image_url);
+            })
+            promise.catch((error) => {
+                console.log(error);
+            })
+        const request = getPosts()
+            request.then((posts) => {
+                setPosts(posts.data)
+            })
+            request.catch((error) => {
+                console.log(error);
+            })
+    }, [])
 
     return (
         <Wrapper>
             <Title>timeline</Title>
             <PublishBox>
                 <ProfilePic 
-                    src= ""
-                    alt= ""
+                    src={userImage}
                 />
                 <FormBox />
             </PublishBox>
+            <Posts>
+                {posts.map((post, id) => <PostCard key={id} image_url={post.image_url} username={post.name} description={post.description} link={post.link}/>)}
+            </Posts>
         </Wrapper>
     )
 }
@@ -27,17 +51,17 @@ const Wrapper = styled.div`
     background-color: var(--background-gray);
     display: flex;
     flex-direction: column;
-    align-items: center;
+    align-items: flex-start;
     justify-content: center;
     h2 {
         font-size: 60px;
-        width: 35vw;
         text-align: left;
     }
 `
 
 const PublishBox = styled.div`  
-    margin-top: 30px;
+    margin-top: 60px;
+    margin-bottom: 15px;
     display: flex;
     background-color: var(--main-white);
     height: fit-content;
@@ -51,5 +75,13 @@ const PublishBox = styled.div`
         margin-left: 18px;
         background-color: red;
     }
+`
+
+const Posts = styled.div`
+    margin-top: 15px;
+    height: fit-content;
+    width: 35vw;
+    min-width: 600px;
+    min-height: fit-content;
 `
 
