773ed3167df4769aabc7fa57e032aae7f84f7030
Nathalia <nathalia.gramst@gmail.com>
2022-10-20 22:20:11 -0300

    Logout communication with api. Closing menu works properly

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,11 @@ import ProfilePic from "../../assets/styles/ProfilePic";
 import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
 import { SearchPeople } from "../Search/Search";
 import Logout from "./Logout";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export function Header () {
-    const [openLogout, setOpenLogout] = useState(false);
+    const [open, setOpen] = useState(false);
+    const menuRef = useRef();
 
     return (
         <Wrapper>
@@ -15,16 +16,15 @@ export function Header () {
 
             <SearchPeople />
 
-            <div>
-                {openLogout ? (
-                    <IoIosArrowUp onClick={() => setOpenLogout(false)} />
+            <div ref={menuRef} onClick={open ? () => {setOpen(false)} : () => {setOpen(true)}}>
+                {open ? (
+                    <IoIosArrowUp />
                 ) : (
-                    <IoIosArrowDown onClick={() => setOpenLogout(true)} />
+                    <IoIosArrowDown />
                 )} 
                 <ProfilePic src="https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q95/wp-content/uploads/2017/06/legiao_nfoMvIZzlwUOC9ESNXdPT4ReLDqpyc15hsV0bmA8Br.png.jpeg"/>
-                <Logout open={openLogout} />
+                <Logout open={open} setOpen={setOpen} menuRef={menuRef} />
             </div>
-
         </Wrapper>
     );
 }
@@ -50,6 +50,7 @@ const Wrapper = styled.header`
         align-items: center;
         position: relative;
         height: 100%;
+        cursor: pointer;
     }
 
     div img {
@@ -62,6 +63,5 @@ const Wrapper = styled.header`
         color: var(--main-white);
 
         margin-right: 15px;
-        cursor: pointer;
     }
 `
--- src/components/Header/Logout.js
+++ src/components/Header/Logout.js
@@ -1,9 +1,32 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { logout } from "../../services/api";
 
-export default function Logout({ open = false }) {
+export default function Logout({ open, setOpen, menuRef }) {
+    const navigate = useNavigate();
+
+
     function handleClick() {
-
+        logout()
+            .then(() => {
+                localStorage.removeItem("linkr");
+                navigate("/");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
+
+    useEffect(() => {
+        if (open) {
+            document.addEventListener("mousedown", (e) => {
+                if (!menuRef.current.contains(e.target)) {
+                    setOpen(false);
+                }
+            });
+        }
+    }, [open]);
 
     return (
         <Wrapper
@@ -27,7 +50,7 @@ const Wrapper = styled.button`
     height: 47px;
     max-height: ${props => props.open ? "100%" : "0"};
     overflow: hidden;
-    transition: all 0.3s ease-out;
+    transition: all 0.1s ease-out;
     position: absolute;
     top: 70px;
     right: -45px;
